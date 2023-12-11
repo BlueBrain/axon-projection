add932916f7131ad63019290a5fdda023e11589a
petkantc <remy.petkantchin@epfl.ch>
2023-12-11 13:34:18 +0100

    Formated code

--- axon_projection/run.py
+++ axon_projection/run.py
@@ -1,11 +1,14 @@
+"""Runs the workflow of extracting axonal projections of given morphologies and classifying them."""
+
+import configparser
+import logging
+import sys
+import time
+
 from axonal_projections import main as create_ap_table
 from check_atlas import compare_source_regions
 from classify_axons import run_classification as classify_axons
 from visualize_connections import create_conn_graphs
-import configparser
-import sys
-import logging
-import time
 
 if __name__ == "__main__":
     start_time = time.time()
@@ -29,4 +32,6 @@ if __name__ == "__main__":
     create_conn_graphs(config)
 
     run_time = time.time() - start_time
-    logging.info("Done in %.2f s = %.2f min = %.2f h.", run_time, run_time/60., run_time/3600.)
+    logging.info(
+        "Done in %.2f s = %.2f min = %.2f h.", run_time, run_time / 60.0, run_time / 3600.0
+    )
